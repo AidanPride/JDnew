80c922aa5819969746c5bf3c587e3f0aca1f5e36
a.aidan@yandex.ru <Aa7030666>
2016-03-17 11:16:57 +0200

    2D grafics window

---
+++ lesson5/Window.java
@@ -0,0 +1,14 @@
+package lesson5;
+
+import javax.swing.*;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class Window {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setBounds(300 , 100 , 800, 600);
+        window.setVisible(true);
+    }
+}
